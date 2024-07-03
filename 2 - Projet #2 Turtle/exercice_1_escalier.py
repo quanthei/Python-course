d1908c970ead0647885ed930767096bd55772966
quanthei <thomascomte.pro@gmail.com>
2024-07-03 18:52:22 +0000

    escalier done

--- 2 - Projet #2 Turtle/exercice_1_escalier.py
+++ 2 - Projet #2 Turtle/exercice_1_escalier.py
@@ -1,49 +1,47 @@
-# 30 pixels
-# faire une fonction
 import turtle
-#Creat the visual env
 t = turtle.Turtle()
-NB_OF_PIXELS_PER_SCALE = 30
+
+NB_PIXELS_PER_SCALE = 30
 nb_scales = 0
-#------------------------------FUNCTIONS------------------------------
-def ask_user_nb_scales() -> int:
+
+#---------------------------------FUNCTIONS---------------------------------
+def ask_nb_scales() -> int:
     answer_nb_scales = 0
     while answer_nb_scales == 0:
-        answer_nb_scales = input("Merci de rentrer le nombres d'escaliers que vous souhaitez dessiner: ")
-        try: 
+        answer_nb_scales = input("Combien de marches pour votre escalier: ")
+        try:
             answer_nb_scales = int(answer_nb_scales)
         except:
-            print("Merci de rentrer uniquement un nombre entiers !")
+            answer_nb_scales = 0
+            print("Merci d'indiquer uniquement un nombre entier !") 
     return answer_nb_scales
 
-def create_a_scale(
-                  isStartUp : bool = False 
-                  ):
-    #Finish/create the base of the scale
-    if not isStartUp: 
-        t.forward(NB_OF_PIXELS_PER_SCALE/4)
-    else:
-        t.forward(NB_OF_PIXELS_PER_SCALE/2)
-    #Position face up
-    t.left(90)
-    #Scale up
-    t.forward(NB_OF_PIXELS_PER_SCALE/2)
-    #Position face right
-    t.right(90)
-    #Base of the next scale
-    t.forward(NB_OF_PIXELS_PER_SCALE/4)
-#------------------------------MAIN------------------------------
-def main():
-    #Ask the user about the numbers of scales he went to introduce
-    nb_scales = ask_user_nb_scales()
+def create_scale(
+                nb_of_scales : int
+                ):
+    for i in range(0, nb_of_scales):
+        #Create/finish the scale base
+        #First round
+        if i == 0: t.forward(NB_PIXELS_PER_SCALE)
+        #Face up
+        t.left(90)
+        #Line up
+        t.forward(NB_PIXELS_PER_SCALE)
+        #Face right
+        t.right(90)
+        #Continue next scale
+        t.forward(NB_PIXELS_PER_SCALE)
 
-    #Process the numbers of scales
-    for i in range(0,nb_scales+1):
-        if i == 0:
-            create_a_scale(True)
-        else:
-            create_a_scale()
-    #Stop the turtle movement
-    t.done()
-#------------------------------EXE------------------------------
+#---------------------------------MAIN---------------------------------
+def main():
+    #Demande à l'utilisateur le nombre de scales qu'il souhaite créer
+    nb_scales = ask_nb_scales()
+    
+    #On creer l'escalier avec le nombre de marches correspondantes
+    create_scale(nb_scales)
+    
+    #Figer le programme
+    turtle.done()
+
+#---------------------------------EXE---------------------------------
 main()
