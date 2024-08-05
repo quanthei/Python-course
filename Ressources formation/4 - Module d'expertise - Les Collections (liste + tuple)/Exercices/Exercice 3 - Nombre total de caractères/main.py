03385ca2c6c581f8432723a049721aee4cec6014
quanthei <thomascomte.pro@gmail.com>
2024-08-05 20:53:59 +0000

    com exo 3

--- Ressources formation/4 - Module d'expertise - Les Collections (liste + tuple)/Exercices/Exercice 3 - Nombre total de caractères/main.py
+++ Ressources formation/4 - Module d'expertise - Les Collections (liste + tuple)/Exercices/Exercice 3 - Nombre total de caractères/main.py
@@ -3,16 +3,16 @@
 #          0        1        2           3           4      5
 noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe", "Martin"]
 
-# 1 - for / len
+# 1 - for / len --> le moins gourmant en mémoire
 sum_caracters = 0
 for nom in noms:
     sum_caracters += len(nom)
 print(f"Nombres total de caractères: {sum_caracters}")
     
-#2 - Completion de liste + sum
+#2 - Completion de liste + sum --> très gourmant en mémoire 
 sum_caracters_2 = sum([len(nom) for nom in noms])
 print(f"Nombres total de caractères: {sum_caracters_2}")
 
-# 3 - Join / len
+# 3 - Join / len --> moyennement gourmand + simplicité d'écriture
 noms_join = "".join(noms)
 print(f"Nombres total de caractères: {len(noms_join)}")
