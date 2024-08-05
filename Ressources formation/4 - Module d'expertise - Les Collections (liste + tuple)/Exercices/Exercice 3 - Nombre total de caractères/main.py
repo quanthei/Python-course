19f632de1a3a0c6fbc3914484679aafc205d0edc
quanthei <thomascomte.pro@gmail.com>
2024-08-05 20:52:18 +0000

    module 4 - exo3 - done

--- Ressources formation/4 - Module d'expertise - Les Collections (liste + tuple)/Exercices/Exercice 3 - Nombre total de caractères/main.py
+++ Ressources formation/4 - Module d'expertise - Les Collections (liste + tuple)/Exercices/Exercice 3 - Nombre total de caractères/main.py
@@ -4,9 +4,15 @@
 noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe", "Martin"]
 
 # 1 - for / len
-
-
-# 2 - Completion de liste + sum
+sum_caracters = 0
+for nom in noms:
+    sum_caracters += len(nom)
+print(f"Nombres total de caractères: {sum_caracters}")
+    
+#2 - Completion de liste + sum
+sum_caracters_2 = sum([len(nom) for nom in noms])
+print(f"Nombres total de caractères: {sum_caracters_2}")
 
 # 3 - Join / len
-
+noms_join = "".join(noms)
+print(f"Nombres total de caractères: {len(noms_join)}")
