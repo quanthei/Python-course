663899c391a4d0704c2b50dbf0a157b057f2b725
quanthei <thomascomte.pro@gmail.com>
2024-08-05 20:37:16 +0000

    exercice 3

---
+++ Ressources formation/4 - Module d'expertise - Les Collections (liste + tuple)/Exercices/Exercice 3 - Nombre total de caractères/main.py
@@ -0,0 +1,12 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Exercice "Nombre total de caractères"
+#          0        1        2           3           4      5
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe", "Martin"]
+
+# 1 - for / len
+
+
+# 2 - Completion de liste + sum
+
+# 3 - Join / len
+
