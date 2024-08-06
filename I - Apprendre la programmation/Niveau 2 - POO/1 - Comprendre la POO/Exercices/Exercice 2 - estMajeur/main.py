97597336c660ae2ad99cd2af6c8e3580f8dd5b1d
quanthei <thomascomte.pro@gmail.com>
2024-08-06 15:52:38 +0000

    update

--- I - Apprendre la programmation/Niveau 2 - POO/1 - Comprendre la POO/Exercices/Exercice 2 - estMajeur/main.py
+++ I - Apprendre la programmation/Niveau 2 - POO/1 - Comprendre la POO/Exercices/Exercice 2 - estMajeur/main.py
@@ -7,21 +7,20 @@ class Personne:
 
     def SePresenter(self):
         print(f"Bonjour je m'appelle {self.nom}", f"J'ai {self.age} ans", sep="\n")
-    
+
+        if self.EstMajeur():
+            print(f"{personne1.nom} est majeur !")
+        else:
+            print(f"{personne1.nom} est mineur !")
+                
     def DemanderNom():
-        pass    
+        pass 
 
     # EstMajeur -> True/False
     def EstMajeur(self) -> bool:
-        if self.age >= 18:
-            return True
-        return False
+        return self.age >= 18
     
 # --- UTILISATION ---
 personne1 = Personne("Thomas", 24) # Je crée une personne
 personne1.SePresenter()
 
-if personne1.EstMajeur():
-    print(f"{personne1.nom} est majeur !")
-else:
-    print(f"{personne1.nom} est mineur !")
