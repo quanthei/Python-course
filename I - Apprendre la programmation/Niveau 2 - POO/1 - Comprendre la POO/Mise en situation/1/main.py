937f9e4c5cd46da602f2294491be70436676d1b4
quanthei <thomascomte.pro@gmail.com>
2024-08-07 09:58:07 +0000

    mise en situation 1

--- I - Apprendre la programmation/Niveau 2 - POO/1 - Comprendre la POO/Mise en situation/1/main.py
+++ I - Apprendre la programmation/Niveau 2 - POO/1 - Comprendre la POO/Mise en situation/1/main.py
@@ -3,34 +3,48 @@
 #   False : Femme
 #   True  : Homme
 class Personne:
-    def __init__(self, nom: str, age: int, genre: bool):
-        self.nom = nom   # crée une variable d'instance : nom
+    def __init__(self, nom: str = "", age: int = 0, genre: bool = None):
+        if nom == "":
+            nom = self.DemanderNom()
+        
+        self.nom = nom  
         self.age = age
         self.genre = genre
-        print("Constructeur personne " + self.nom)
 
     def SePresenter(self):
-        # Bonjour, je m'appelle Jean, j'ai 30 ans
-        # Je suis majeur
-        if self.genre:
-            print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
-            print("Genre : Masculin")
-            if self.EstMajeur():
-                print("Je suis majeur")
-            else:
-                print("Je suis mineur")
-            print()
-        else:
-            print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
-            print("Genre : Feminin")
-            if self.EstMajeur():
-                print("Je suis majeure")
-            else:
-                print("Je suis mineure")
-            print()
+        # Afficher âge + prénom
+        infos_personne = f"Bonjour, je m'appelle {self.nom}." 
+        if self.age != 0:
+            infos_personne += f" J'ai {self.age} ans."
 
-    def EstMajeur(self):
-        return self.age >= 18
+        print(infos_personne)
+
+        # Afficher genre 
+        if self.genre == None: 
+            print("Genre: Non précisé")
+        elif self.genre: # Je suis un Homme
+            print("Genre : Masculin")
+        else: # Je suis une Femme
+            print("Genre : Feminin")
+    
+        # Mineur/Majeur
+        print(self.EstMajeur())
+
+    def EstMajeur(self) -> bool:
+        infos_personne = "Je suis "
+        if self.age >= 18:
+            infos_personne += "majeur"
+        else:
+            infos_personne += "mineur"
+        if not self.genre: # Si on sûr et certain que la personne est une femme on ajoute un 'e'
+            infos_personne += "e"
+
+        return infos_personne
+
+    def DemanderNom(self) -> str:
+        while nom == "":
+            nom = input("Merci d'indiquer votre prénom: ")
+        return nom
 
 personne1 = Personne("Jean", 25)
 personne1.SePresenter()
