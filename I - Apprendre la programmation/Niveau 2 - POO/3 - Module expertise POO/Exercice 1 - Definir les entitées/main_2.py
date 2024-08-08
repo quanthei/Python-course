4eeb70d5092e95ccc1fb6de8df0d63945ba5d9a8
quanthei <thomascomte.pro@gmail.com>
2024-08-08 15:10:41 +0000

    main 2 - a corriger

---
+++ I - Apprendre la programmation/Niveau 2 - POO/3 - Module expertise POO/Exercice 1 - Definir les entitées/main_2.py
@@ -0,0 +1,58 @@
+class Question():
+    id_question = 0
+
+    def __init__(self, question: str, answers: tuple, correct_answer: str):
+        self.question = question
+        self.answers = answers
+        self.id_min_answer = 1
+        self.id_max_answer = len(self.answers)
+        self.user_answer = ""
+        self.id_correct_answer = self.answers.index(correct_answer) + 1
+        self.score = 0
+    
+    def AskQuestion(self):
+        Question.id_question +=1
+
+        print()
+        print(f"QUESTION NUMÉRO {Question.id_question}")
+        print()
+        print(self.question)
+    
+        for answer in self.answers:
+            print("  ", self.answers.index(answer) + 1 , "-", answer)
+        print()
+
+        # Ask user for his answer
+        self.user_answer = self.AskUserAnswer()
+
+        # Increment score if user answer is correct
+        if self.UserAnswerIsCorrect(): 
+            print("Bonne réponse !")
+            self.IncrementScore()
+            print(f"Votre score est de {self.score}/{len(questionnaire)}")
+
+    def AskUserAnswer(self) -> int:
+        user_answer_str = input(f"Votre réponse (entre {self.id_min_answer} et {self.id_max_answer}): ")
+        try:
+            reponse_int = int(user_answer_str)
+            if min <= reponse_int <= max:
+                return reponse_int
+            print(f"ERREUR : Vous devez rentrer un nombre entre {self.id_min_answer} et {self.id_max_answer}")
+        except:
+            print("ERREUR : Veuillez rentrer uniquement des chiffres !")
+        return self.AskUserAnswer()
+
+    def UserAnswerIsCorrect(self) -> bool:
+        return True if self.id_correct_answer == self.user_answer else False
+
+    def IncrementScore(self):
+        self.score += 1
+
+questionnaire = (
+                Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
+                Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
+                Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
+                )
+
+for question in questionnaire:
+    question.AskQuestion()
