cd4fcaffb771020814df93b87dd2da07ace443d6
quanthei <thomascomte.pro@gmail.com>
2024-08-13 14:45:21 +0000

    horse

--- I - Apprendre la programmation/Niveau 3 - Les algorithmes avec Python/1 - Les challenges de code/Chevaux de course/main.py
+++ I - Apprendre la programmation/Niveau 3 - Les algorithmes avec Python/1 - Les challenges de code/Chevaux de course/main.py
@@ -1,63 +1,36 @@
-import sys
-import math
-import copy
+class Course():
+    def __init__(self, skills: list = []):
+        self.skills = skills
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-# ---
-# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    def IdentifyMinSkillDeltaForDuel(self):
+        delta = 500000
+        skill_before = 0
+        for skill in self.skills:
+            delta_temp = skill - skill_before
+            if delta_temp < delta:
+                delta = delta_temp
+        return delta
 
-# light_x: the X position of the light of power
-# light_y: the Y position of the light of power
-# initial_tx: Thor's starting X position
-# initial_ty: Thor's starting Y position
-light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
-
-# Positions
-LIGHTER_INITIAL_POSITION = (light_x, light_y)
-thor_actual_position = [initial_tx,initial_ty]
-thor_position_temp = [0,0]
-
-# Turn
-actual_turn = 0
-
-def north_or_south(): # Y
-    if LIGHTER_INITIAL_POSITION[1] == thor_actual_position[1]:
-        return ""
-    else:
-        thor_position_temp[1] += 1
-        if LIGHTER_INITIAL_POSITION[1] > thor_actual_position[1]:
-            return "N"
-    return "S"
-
-def west_or_est(): # X
-    if LIGHTER_INITIAL_POSITION[0] == thor_actual_position[0]:
-        return ""
-    else:
-        thor_position_temp[0] += 1
-        if LIGHTER_INITIAL_POSITION[0] > thor_actual_position[0]:
-            return "E"
-    return "W"
-
-# game loop
-while True:
-    instructions = "" # clean des instructions
-
-    actual_turn += 1
-    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
-
-    if actual_turn == 1: # First init
-        thor_position_temp = copy.copy(thor_actual_position)
-
-    # Definir north ou south via Y
-    instructions += north_or_south()
-
-    # Definir east ou west via X
-    instructions += west_or_est()
-
-    # New thor_actual_position
-    thor_actual_position = copy.copy(thor_position_temp)
+class Horse():
+    def __init__(self, puissance: int):
+        self.puissance = puissance
     
-    # A single line providing the move to be made: N NE E SE S SW W or NW
-    print(instructions)
+    @staticmethod
+    def GetHorseInfos() -> int:
+        return int(input())
+
+# We get the numbers of horses
+nb_horses = int(input())
+horses = []
+
+# We get all the horses infos and stock them into the horses list
+for horse in range(nb_horses):
+    horse_infos = Horse.GetHorseInfos()
+    horses.append(Horse(horse_infos))
+
+print(horses, file=sys.stderr, flush=True)
+
+course_horses = Course(horses.sort(reverse=True))
+delta_min = course_horses.IdentifyMinSkillDeltaForDuel()
+
+print(delta_min)
